96aab8e0e8e68247791c266e6f206997049b73b5
dhruv.gohil@ishisystems.com <dhruv.gohil@ishisystems.com>
2016-02-07 13:20:20 +0530

    Reduced more things

--- src/main/java/com/meetup/App.java
+++ src/main/java/com/meetup/App.java
@@ -1,11 +1,7 @@
 package com.meetup;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-import java.util.TimeZone;
 
 /**
  * The Application
